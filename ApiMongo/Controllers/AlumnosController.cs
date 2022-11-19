f958091077a4f01cde823292b4212e328f6cd00e
Rodrivers <rogerleon400@gmail.com>
2022-11-19 11:24:15 -0600

    Agregar archivos de proyecto.

---
+++ ApiMongo/Controllers/AlumnosController.cs
@@ -0,0 +1,45 @@
+﻿using ApiMongo.Models;
+using ApiMongo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZstdSharp.Unsafe;
+
+namespace ApiMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnosController : ControllerBase
+    {
+        public EscuelaServices _alumnoService;
+
+        public AlumnosController(EscuelaServices alumnoService)
+        {
+            _alumnoService = alumnoService;
+
+        }
+        [HttpPost]
+
+        public ActionResult<Alumnos> Create(Alumnos alumnos)
+        {
+            _alumnoService.Create(alumnos);
+            return Ok();
+
+        }
+        [HttpPut]
+
+        public ActionResult Update(Alumnos alumnos)
+        {
+            _alumnoService.Update(alumnos.Id, alumnos);
+            return Ok();
+        }
+
+        [HttpDelete("(id)")]
+
+        public ActionResult Delete(string id)
+        {
+            _alumnoService.Delete(id);
+            return Ok();
+
+        }
+    }
+}
